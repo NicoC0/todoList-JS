d3727768da0f5c121cce417ef094f3d0ec57b9dc
NicoC0 <nicoconfaa@gmail.com>
2022-01-18 05:22:23 -0300

    add css styles and date section

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Time
+const dateNumber = document.getElementById('dateNumber')
+const dateText = document.getElementById('dateText')
+const dateMonth = document.getElementById('dateMonth')
+const dateYear = document.getElementById('dateYear')
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('es', {day: 'numeric'})
+    dateText.textContent = date.toLocaleString('es', {weekday: 'long'})
+    dateMonth.textContent = date.toLocaleString('es', {month: 'short'})
+    dateYear.textContent = date.toLocaleString('es', {year: 'numeric'})
+    console.log('alo')
+}
+
+setDate()
+
+// Tasks
 const formulario = document.getElementById('formulario')
 const input = document.getElementById('input')
 const listaTarea = document.getElementById('lista-tareas')
